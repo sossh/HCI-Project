9651ec910c55b38cbc51dfd4bd557de349c0fe0e
ahirma <ahirma@myumanitoba.ca>
2025-11-17 19:48:45 -0600

    parking,js JSON fixed to represent whole lots

---
+++ Milestone3/js/publicLots.js
@@ -0,0 +1,324 @@
+export const publicLots = {
+    "ACW Lot": {
+        "map_display_name": "ACW",
+        "area": [
+            [49.809218, -97.1298677],
+            [49.80954, -97.1288914],
+            [49.8101908, -97.1294385],
+            [49.8098896, -97.1303881],
+            [49.809218, -97.1298677]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "AC Lot": {
+        "map_display_name": "AC",
+        "area": [
+            [49.8094026, -97.1286105],
+            [49.8090287, -97.1282404],
+            [49.809323, -97.1274679],
+            [49.8096622, -97.1277522],
+            [49.8094026, -97.1286105]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "D Lot": {
+        "map_display_name": "D",
+        "area": [
+            [49.8065038, -97.1322574],
+            [49.8061749, -97.131973],
+            [49.8063065, -97.1316137],
+            [49.806625, -97.1318819],
+            [49.8065038, -97.1322574]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "M Lot": {
+        "map_display_name": "M",
+        "area": [
+            [49.8049848, -97.134439],
+            [49.8047494, -97.1342566],
+            [49.8050713, -97.1332857],
+            [49.8052998, -97.1334734],
+            [49.8049848, -97.134439]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "G Lot": {
+        "map_display_name": "G",
+        "area": [
+            [49.8057984, -97.1366296],
+            [49.8056391, -97.1364848],
+            [49.8058088, -97.1359913],
+            [49.805975, -97.1361307],
+            [49.8057984, -97.1366296]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "ALC Lot": {
+        "map_display_name": "ALC",
+        "area": [
+            [49.8068748, -97.1377857],
+            [49.8065459, -97.1375067],
+            [49.8067986, -97.136745],
+            [49.8071275, -97.1370293],
+            [49.8068748, -97.1377857]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "H Lot": {
+        "map_display_name": "H",
+        "area": [
+            [49.8079438, -97.1337269],
+            [49.807801, -97.1336116],
+            [49.8079403, -97.1332052],
+            [49.8080797, -97.133334],
+            [49.8079438, -97.1337269]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "U Lot": {
+        "map_display_name": "U",
+        "area": [
+            [49.8062903, -97.1419932],
+            [49.8060531, -97.1418108],
+            [49.8067126, -97.1399279],
+            [49.8069307, -97.1401156],
+            [49.8062903, -97.1419932]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "SD West Lot": {
+        "map_display_name": "SD West",
+        "area": [
+            [49.8050535, -97.1502512],
+            [49.8045723, -97.1498649],
+            [49.8049289, -97.1488511],
+            [49.8053928, -97.1492427],
+            [49.8050535, -97.1502512]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "SD East Lot": {
+        "map_display_name": "SD East",
+        "area": [
+            [49.8054621, -97.1488296],
+            [49.8046485, -97.1481966],
+            [49.805057, -97.1469628],
+            [49.8058464, -97.147628],
+            [49.8054621, -97.1488296]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "A Lot": {
+        "map_display_name": "A",
+        "area": [
+            [49.8112811, -97.1358854],
+            [49.8107826, -97.1354938],
+            [49.8108623, -97.1352363],
+            [49.8113608, -97.1356279],
+            [49.8112811, -97.1358854]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "V Lot": {
+        "map_display_name": "V",
+        "area": [
+            [49.8096609, -97.1397546],
+            [49.8094358, -97.139583],
+            [49.8095397, -97.1392504],
+            [49.8097612, -97.1394221],
+            [49.8096609, -97.1397546]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "Q Lot": {
+        "map_display_name": "Q",
+        "area": [
+            [49.8105419, -97.1397609],
+            [49.8104312, -97.139675],
+            [49.8110681, -97.1378511],
+            [49.811165, -97.1379262],
+            [49.8105419, -97.1397609]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "L Lot": {
+        "map_display_name": "L",
+        "area": [
+            [49.8123315, -97.129628],
+            [49.8113864, -97.1289145],
+            [49.8116184, -97.1281849],
+            [49.812463, -97.1288769],
+            [49.8123315, -97.129628]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "Parkade": {
+        "map_display_name": "Parkade",
+        "area": [
+            [49.8096305, -97.136576],
+            [49.8091424, -97.1361791],
+            [49.8094228, -97.1352779],
+            [49.8099283, -97.1356856],
+            [49.8096305, -97.136576]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "B.Public Lot": {
+        "map_display_name": "B (Public)",
+        "area": [
+            [49.8111745, -97.1292292],
+            [49.810593, -97.1287464],
+            [49.8106968, -97.1284889],
+            [49.8112472, -97.1290307],
+            [49.8111745, -97.1292292]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "Stores Lot": {
+        "map_display_name": "Stores",
+        "area": [
+            [49.8053874, -97.1337929],
+            [49.8053182, -97.1337313],
+            [49.8053744, -97.133569],
+            [49.8054428, -97.133628],
+            [49.8053874, -97.1337929]
+        ],
+        "isVisitor": true,
+        "isStudent": false,
+        "isFaculty": false,
+        "isStaff": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    }
+};
---
+++ Milestone3/js/staffLots.js
@@ -0,0 +1,60 @@
+export const staffLots = {
+    "B1 Lot": {
+        "map_display_name": "B1",
+        "area": [
+            [49.8109535, -97.1299023],
+            [49.8103546, -97.129441],
+            [49.8105589, -97.1288348],
+            [49.8111508, -97.1293122],
+            [49.8109535, -97.1299023]
+        ],
+        "isStaff": true,
+        "isVisitor": false,
+        "isStudent": false,
+        "isFaculty": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "K Lot": {
+        "map_display_name": "K",
+        "area": [
+            [49.8121019, -97.1377414],
+            [49.811368, -97.1371782],
+            [49.8115272, -97.1367356],
+            [49.8119928, -97.1371406],
+            [49.8121019, -97.1377414]
+        ],
+        "isStaff": true,
+        "isVisitor": false,
+        "isStudent": false,
+        "isFaculty": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    "A Lot": {
+        "map_display_name": "A",
+        "area": [
+            [49.8107112, -97.1353884],
+            [49.8103234, -97.1350987],
+            [49.8104031, -97.1348359],
+            [49.8107942, -97.1351148],
+            [49.8107112, -97.1353884]
+        ],
+        "isStaff": true,
+        "isVisitor": false,
+        "isStudent": false,
+        "isFaculty": false,
+        "isCovered": false,
+        "isElectric": false,
+        "isAccessible": true,
+        "isAvailable": true
+    },
+
+    // Continue for ALL staff lots exactly in the same cleaned format...
+};
